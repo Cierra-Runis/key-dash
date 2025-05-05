7f25984fdc032a6ebd85d2993b395f74a5037c4b
Cierra-Runis <byrdsaron@gmail.com>
2025-05-05 21:29:11 +0800

    feat: Add I18n

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,23 @@ use ratatui::{
     widgets::{Block, Paragraph},
 };
 
+// Load I18n macro, for allow you use `t!` macro in anywhere.
+#[macro_use]
+extern crate rust_i18n;
+
+// Init translations for current crate.
+// This will load Configuration using the `[package.metadata.i18n]` section in `Cargo.toml` if exists.
+// Or you can pass arguments by `i18n!` to override it.
+i18n!("locales");
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+
+    // Initialize language
+    let locale = sys_locale::get_locale().unwrap_or_else(|| "en".to_string());
+    rust_i18n::set_locale(&locale);
+
+    // Initialize the terminal
     let terminal = ratatui::init();
     let result = App::new().run(terminal);
     ratatui::restore();
@@ -45,13 +60,8 @@ impl App {
     /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
     /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
     fn render(&mut self, frame: &mut Frame) {
-        let title = Line::from("Ratatui Simple Template")
-            .bold()
-            .blue()
-            .centered();
-        let text = "Hello, Ratatui!\n\n\
-            Created using https://github.com/ratatui/templates\n\
-            Press `Esc`, `Ctrl-C` or `q` to stop running.";
+        let title = Line::from(t!("app.name")).bold().blue().centered();
+        let text = t!("app.description");
         frame.render_widget(
             Paragraph::new(text)
                 .block(Block::bordered().title(title))
